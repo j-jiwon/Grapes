1206c214100e5a9aa091ba3d5f6d865ccff81146
j-jiwon <ziwon16@gmail.com>
2022-07-12 00:54:42 +0900

    Add glfw library, Update dependency,cmake

--- Dependency.cmake
+++ Dependency.cmake
@@ -6,6 +6,7 @@ set(DEP_INSTALL_DIR ${PROJECT_BINARY_DIR}/install)
 set(DEP_INCLUDE_DIR ${DEP_INSTALL_DIR}/include)
 set(DEP_LIB_DIR ${DEP_INSTALL_DIR}/lib)
 
+# spdlog
 Externalproject_Add(
     dep-spdlog
     GIT_REPOSITORY "https://github.com/gabime/spdlog.git"
@@ -16,3 +17,23 @@ Externalproject_Add(
     CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${DEP_INSTALL_DIR}
     TEST_COMMAND ""
 )
+set(DEP_LIST ${DEP_LIST} dep-spdlog)
+set(DEP_LIBS ${DEP_LIBS} spdlog$<$<CONFIG:Debug>:d>)
+
+# glfw
+Externalproject_Add(
+    dep_glfw
+    GIT_REPOSITORY "https://github.com/glfw/glfw.git"
+    GIT_TAG "3.3.2"
+    GIT_SHALLOW 1
+    UPDATE_COMMAND "" 
+    PATCH_COMMAND "" 
+    TEST_COMMAND ""
+    CMAKE_ARGS
+        -DCMAKE_INSTALL_PREFIX=${DEP_INSTALL_DIR}
+        -DGLFW_BUILD_EXAMPLES=OFF
+        -DGLFW_BUILD_TESTS=OFF
+        -DGLFW_BUILD_DOCS=OFF
+)
+set(DEP_LIST ${DEP_LIST} dep_glfw)
+set(DEP_LIBS ${DEP_LIBS} glfw3)
