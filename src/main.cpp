89d7028cdfaf2e3b511d769675b079e169267897
j-jiwon <ziwon16@gmail.com>
2022-07-12 00:56:53 +0900

    create and initialize glfw window

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,35 @@
 #include <spdlog/spdlog.h>
+#include <GLFW/glfw3.h>
 
 int main(int args, const char** argv)
 {
-    SPDLOG_ERROR("hello world");
+    SPDLOG_INFO("start program");
+
+    SPDLOG_INFO("init glfw");
+    if (!glfwInit())
+    {
+        const char* description = nullptr;
+        glfwGetError(&description);
+        SPDLOG_ERROR("failed to init glfw: {}", description);
+        return -1;
+    }
+
+    SPDLOG_INFO("create glfw window");
+    auto window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_NAME, nullptr, nullptr);
+    if (!window)
+    {
+        SPDLOG_ERROR("failed to create glfw window");
+        glfwTerminate();
+        return -1;
+    }
+
+    SPDLOG_INFO("start main loop");
+    while(!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+
     return 0;
 }
