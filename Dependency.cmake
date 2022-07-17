82c3f2bc16109585babc786cf7263bdfbd722c7d
j-jiwon <ziwon16@gmail.com>
2022-07-18 01:39:04 +0900

    add glad, init window with OpenGL func

--- Dependency.cmake
+++ Dependency.cmake
@@ -37,3 +37,18 @@ Externalproject_Add(
 )
 set(DEP_LIST ${DEP_LIST} dep_glfw)
 set(DEP_LIBS ${DEP_LIBS} glfw3)
+
+# glad
+ExternalProject_Add(
+    dep_glad
+    GIT_REPOSITORY "https://github.com/Dav1dde/glad"
+    GIT_TAG "v0.1.34"
+    GIT_SHALLOW 1
+    UPDATE_COMMAND ""
+    PATCH_COMMAND ""
+    CMAKE_ARGS
+        -DCMAKE_INSTALL_PREFIX=${DEP_INSTALL_DIR}
+    TEST_COMMAND ""
+    )
+set(DEP_LIST ${DEP_LIST} dep_glad)
+set(DEP_LIBS ${DEP_LIBS} glad)
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,31 @@
 #include <spdlog/spdlog.h>
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
+void OnFramebufferSizeChange(GLFWwindow* window, int width, int height)
+{
+    SPDLOG_INFO("framebuffer size: {} x {}", width, height);
+    glViewport(0, 0, width, height);
+}
+
+void OnKeyEvent(GLFWwindow* window, int key, int scancode, int action, int mods)
+{   
+    SPDLOG_INFO("key: {}, scancod: {}, action: {}, mods: {}{}{}",key, scancode,
+                action==GLFW_PRESS ? "Pressed" :
+                action==GLFW_RELEASE ? "Released" :
+                action==GLFW_REPEAT ? "Repeat" : "Unknowns",
+                mods&GLFW_MOD_CONTROL ? "C" : "-",
+                mods&GLFW_MOD_SHIFT ? "S" : "-",
+                mods&GLFW_MOD_ALT ? "A" : "-");
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+void Render()
+{
+    glClearColor(1.0f, 1.0f, 0.0f, 0.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+}
 
 int main(int args, const char** argv)
 {
@@ -14,6 +40,10 @@ int main(int args, const char** argv)
         return -1;
     }
 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
     SPDLOG_INFO("create glfw window");
     auto window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_NAME, nullptr, nullptr);
     if (!window)
@@ -22,11 +52,27 @@ int main(int args, const char** argv)
         glfwTerminate();
         return -1;
     }
+    glfwMakeContextCurrent(window);
+    
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        SPDLOG_ERROR("failed to initialize glad");
+        glfwTerminate();
+        return -1;
+    }
+    auto glVersion = glGetString(GL_VERSION);
+    SPDLOG_INFO("OpenGL context version: {}", glVersion);
+    
+    OnFramebufferSizeChange(window, WINDOW_WIDTH, WINDOW_HEIGHT);
+    glfwSetFramebufferSizeCallback(window, OnFramebufferSizeChange);
+    glfwSetKeyCallback(window, OnKeyEvent);
 
     SPDLOG_INFO("start main loop");
     while(!glfwWindowShouldClose(window))
     {
         glfwPollEvents();
+        Render();
+        glfwSwapBuffers(window);
     }
 
     glfwTerminate();
