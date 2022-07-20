171cf2a6d767fe0a66d0781233a90d1d0007ab8a
j-jiwon <ziwon16@gmail.com>
2022-07-21 01:04:17 +0900

    add common

---
+++ src/common.h
@@ -0,0 +1,13 @@
+#ifndef __COMMON_H__
+#define __COMMON_H__
+
+#include <memory>
+#include <string>
+#include <optional>
+#include <glad/glad.h>
+#include <glfw/glfw3.h>
+#include <spdlog/spdlog.h>
+
+std::optional<std::string> LoadTextFile(const std::string& filename);
+
+#endif 
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include "common.h"
 #include <spdlog/spdlog.h>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
