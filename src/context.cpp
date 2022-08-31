f56ede1d5a8bbabff0a59146796baffd3f5a56e8
j-jiwon <ziwon16@gmail.com>
2022-09-01 02:43:32 +0900

    multiple rotating cubes

--- src/context.cpp
+++ src/context.cpp
@@ -14,20 +14,36 @@ void Context::Render() {
     // program->Use();
     // glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
 
+  std::vector<glm::vec3> cubePositions = {
+        glm::vec3( 0.0f, 0.0f, 0.0f),
+        glm::vec3( 2.0f, 5.0f, -15.0f),
+        glm::vec3(-1.5f, -2.2f, -2.5f),
+        glm::vec3(-3.8f, -2.0f, -12.3f),
+        glm::vec3( 2.4f, -0.4f, -3.5f),
+        glm::vec3(-1.7f, 3.0f, -7.5f),
+        glm::vec3( 1.3f, -2.0f, -2.5f),
+        glm::vec3( 1.5f, 2.0f, -2.5f),
+        glm::vec3( 1.5f, 0.2f, -1.5f),
+        glm::vec3(-1.3f, 1.0f, -1.5f),
+    };
+
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glEnable(GL_DEPTH_TEST);
-
     auto projection = glm::perspective(glm::radians(45.0f),
-        (float)WINDOW_WIDTH / (float)WINDOW_HEIGHT, 0.01f, 10.0f);
+        (float)640 / (float)480, 0.01f, 20.0f);
     auto view = glm::translate(glm::mat4(1.0f),
         glm::vec3(0.0f, 0.0f, -3.0f));
-    auto model = glm::rotate(glm::mat4(1.0f),
-        glm::radians((float)glfwGetTime() * 120.0f),
-        glm::vec3(1.0f, 0.5f, 0.0f));
-    auto transform = projection * view * model;
-    program->SetUniform("transform", transform);
 
-    glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+    for (size_t i = 0; i < cubePositions.size(); i++){
+        auto& pos = cubePositions[i];
+        auto model = glm::translate(glm::mat4(1.0f), pos);
+        model = glm::rotate(model,
+            glm::radians((float)glfwGetTime() * 120.0f + 20.0f * (float)i),
+            glm::vec3(1.0f, 0.5f, 0.0f));
+        auto transform = projection * view * model;
+        program->SetUniform("transform", transform);
+        glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+    }
 }
 
 bool Context::Init() {
